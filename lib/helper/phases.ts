02d32d172e5f1c310def1681766dc4f4d783988b
AmiyoKm <games.amiyo@gmail.com>
2025-01-22 18:40:32 +0600

    Update import paths to use generated client in phases and ExecuteWorkflow files

--- lib/helper/phases.ts
+++ lib/helper/phases.ts
@@ -1,4 +1,4 @@
-import { ExecutionPhase } from "@prisma/client";
+import { ExecutionPhase } from "@/prisma/generated/client";
 
 type Phase = Pick<ExecutionPhase , "creditsConsumed">
 export function GetPhasesTotalCost(phases : Phase[]){
