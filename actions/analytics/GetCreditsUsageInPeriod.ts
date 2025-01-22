73670a41f2ac1868cef4bfdaaf8d74c063b12104
AmiyoKm <games.amiyo@gmail.com>
2025-01-22 17:18:55 +0600

    Add type annotation for execution phases in GetCreditsUsageInPeriod function

--- actions/analytics/GetCreditsUsageInPeriod.ts
+++ actions/analytics/GetCreditsUsageInPeriod.ts
@@ -2,6 +2,7 @@
 
 import { PeriodToDateRange } from "@/lib/helper/dates";
 import { prisma } from "@/lib/prisma";
+import { ExecutionPhase } from "@/prisma/generated/client";
 import { Period } from "@/types/analytics";
 import { ExecutionPhaseStatus, WorkflowExecutionStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
@@ -41,7 +42,7 @@ export async function GetCreditsUsageInPeriod(period:Period) {
         }
         return acc
     },{} as any)
-    executionPhases.forEach((phase)=> {
+    executionPhases.forEach((phase : ExecutionPhase)=> {
         const date = format(phase.startedAt! , dateFormat)
         if(phase.status === ExecutionPhaseStatus.COMPLETED){
             stats[date].success += phase.creditsConsumed || 0
